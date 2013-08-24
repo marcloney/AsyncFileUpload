acbec345630a7d8033ed874e3999410d27d470e5
Marc Loney <marc@marcloney.com>
2013-08-24 12:33:36 +0800

    Updated bower.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,16 +2,23 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json')
+  , bower: {
+      all: {
+        dest: './tmp/main.js'
+      , exclude: 'jquery'
+      }
+    }
   , uglify: {
       build: {
         files: {
-          './<%= pkg.name %>-<%= pkg.version %>.min.js': ['./js/main.js']
+          './<%= pkg.name %>-<%= pkg.version %>.min.js': ['./tmp/main.js']
         }
       }
     }
   });
 
+  grunt.loadNpmTashs('grunt-bower-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['bower', 'uglify']);
 };
